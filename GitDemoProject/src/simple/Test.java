fa100f55836ce2ddb1af69246b260768f2a2ec92
Mmasal <142581818+Mmasal@users.noreply.github.com>
2024-10-18 16:30:45 +0530

    first commit

---
+++ GitDemoProject/src/simple/Test.java
@@ -0,0 +1,13 @@
+package simple;
+
+public class Test {
+	public static void main(String[] args) {
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("eat somthing");
+		} else {
+			System.out.println("Do your Work");
+		}
+	}
+}
